2ae4e6799053ba8131a7e62184b00272f9f76e43
schopra8 <schopra8@stanford.edu>
2016-05-04 23:39:57 -0700

    created initial devise admin model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}  
   get ':controller(/:action(/:id))'
   post ':controller(/:action(/:id))'
