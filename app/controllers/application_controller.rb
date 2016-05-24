3d003202a0607689117a9c0ef648ab481e4ee773
schopra8 <schopra8@stanford.edu>
2016-05-23 23:47:47 -0700

    Approval mechanism for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def authorize_admin!
-
    redirect_to({controller: "index", :action => "display"}) unless current_admin
    #redirects to previous page
   end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,19 @@
 class DashboardController < ApplicationController
   before_filter :authorize_admin!
+
+  def display
+  	@users = User.where(approved: false)
+  end
+
+  def authorize_user
+  	user_id = params[:id]
+  	u = User.find_by_id(user_id)
+  	if (u == nil)
+  		not_found
+  		return
+  	end
+  	u.approved = true
+  	u.save()
+  	redirect_to({controller: "dashboard", :action => "display"})
+  end
 end
