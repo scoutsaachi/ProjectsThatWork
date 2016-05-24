e6c9f26d157d1618bb2a99747555b26381b14f46
Saachi Jain <saachi@stanford.edu>
2016-05-23 23:43:12 -0700

    fixed user bug

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,26 @@
 class DashboardController < ApplicationController
   before_filter :authorize_admin!
+
+  	#TODO: need to move to admin
+	def projectApproval
+		@requests = Request.pending
+	end
+
+	#TODO: need to move to admin
+	def post_approverequest
+		requests = Request.pending
+		requests.each do |r|
+			checkLabel = "check#{r.id}"
+			revLabel = "rev#{r.id}"
+			if params[checkLabel] == "2"
+				r.approve_request
+			elsif params[checkLabel] == "3"
+				r.deny_request(params[revLabel])
+			else
+				next
+			end
+		end
+		redirect_to({controller: "dashboard", :action => "projectApproval"})
+	end
+
 end
--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -57,26 +57,4 @@ class RequestController < ApplicationController
 	    redirect_to({controller: "projects", :action => "display_categories", :id => params[:id]})
 	end
 
-	#TODO: need to move to admin
-	def projectApproval
-		@requests = Request.pending
-	end
-
-	#TODO: need to move to admin
-	def post_approverequest
-		requests = Request.pending
-		requests.each do |r|
-			checkLabel = "check#{r.id}"
-			revLabel = "rev#{r.id}"
-			if params[checkLabel] == "2"
-				r.approve_request
-			elsif params[checkLabel] == "3"
-				r.deny_request(params[revLabel])
-			else
-				next
-			end
-		end
-		redirect_to({controller: "request", :action => "projectApproval"})
-	end
-
 end
